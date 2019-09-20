90d555ed95080f9935c7bf862b41eb048c6f2794
yellow <donte_beck@yahoo.com>
2019-09-20 16:40:29 -0700

    it works now

--- server/index.js
+++ server/index.js
@@ -1,26 +1,27 @@
 const path = require('path');
-const app = require('express')();
+const express = require('express');
+const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 const port = process.env.PORT || 3000;
 
-app.get('/test', function(req, res){
-    res.sendFile(path.join(__dirname, '../client/index.html'));
+app.use(express.static(path.join(__dirname, '../client')));
+
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, '../client/index.html'));
 });
 
-io.on('connection', function(socket){
-    console.log('a user connected');
-    socket.on('disconnect', function(){
-      console.log('user disconnected');
-    });
-});
-
-io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
+io.on('connection', function(socket) {
+  console.log('a user connected');
+  socket.on('disconnect', function() {
+    console.log('user disconnected');
+  });
+  socket.on('chat message', function(msg) {
     io.emit('chat message', msg);
+    console.log('user:' + msg);
   });
 });
 
-http.listen(port, function(){
-    console.log(`listening on *:${port}`);
+http.listen(port, function() {
+  console.log(`listening on *:${port}`);
 });
